540bd5730a8d96cd67c93297590141b7109295e5
liyiwen <liyiwen@huoyunren.com>
2018-09-18 14:42:59 +0800

    代码结构调整

--- dht/util/node.go
+++ dht/util/node.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"strconv"
 	"errors"
+	"fmt"
 )
 
 func DecodeCompactIPPortInfo(info string) (ip net.IP, port int, err error) {
@@ -32,6 +33,7 @@ func EncodeCompactIPPortInfo(ip net.IP, port int) (info string, err error) {
 	}
 
 	info = string(append(ip, p...))
+	fmt.Println("EncodeCompactIPPortInfo: ", len(ip), " ", len(info))
 	return
 }
 
